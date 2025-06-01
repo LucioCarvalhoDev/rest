67c39ab7a72c34777fc245195e22b3643ad59ba9
Lucio Carvalho Almeida <carvalholuciodxd@gmail.com>
2025-06-01 15:37:25 -0300

    View::display_dar_balance

---
+++ src/atoms.rs
@@ -0,0 +1,84 @@
+pub mod atoms {
+    use chrono::{DateTime, NaiveDate, Utc};
+
+    #[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
+    pub enum ID {
+        PersonID(i32),
+        AccountID(i32),
+        TransactionID(i32),
+    }
+
+    impl ID {
+        pub fn get_value(&self) -> i32 {
+            return match self {
+                ID::PersonID(id) => *id,
+                ID::AccountID(id) => *id,
+                ID::TransactionID(id) => *id,
+            }
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct IdGenerator {
+        _person: i32,
+        _account: i32,
+        _transaction: i32,
+    }
+
+    impl IdGenerator {
+        pub fn new() -> IdGenerator {
+            IdGenerator {
+                _person: 1,
+                _account: 1,
+                _transaction: 1,
+            }
+        }
+
+        pub fn person(&mut self) -> ID {
+            let id = self._person;
+            self._person += 1;
+            return ID::PersonID(id);
+        }
+
+        pub fn account(&mut self) -> ID {
+            let id = self._account;
+            self._account += 1;
+            return ID::AccountID(id);
+        }
+
+        pub fn transaction(&mut self) -> ID {
+            let id = self._transaction;
+            self._transaction += 1;
+            return ID::TransactionID(id);
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Person {
+        pub name: String,
+        pub id: ID,
+        pub accounts: Vec<i32>
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct Account {
+        pub title: String,
+        pub id: ID,
+        pub owners: Vec<i32>,
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum TransactionRepeatType {
+        EveryDate(String),
+        EveryXDays(i32),
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Transaction {
+        pub id: ID,
+        pub value: f64,
+        pub target_account: i32,
+        pub source_account: Option<i32>,
+        pub date: DateTime<Utc>,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,188 @@
+pub mod atoms;
+
+use atoms::atoms::{ID, IdGenerator, Account, Person, Transaction};
 use std::collections::HashMap;
-use chrono::NaiveDate;
+use chrono::{Datelike, Local, NaiveDate, Utc};
 
-fn main() {
-    #[derive(Debug, Clone)]
-    struct Person {
-        name: String,
-        id: i32,
-    }
-
-    #[derive(Debug, Clone)]
-    struct Account {
-        title: String,
-        id: i32,
-    }
-
-    #[derive(Debug, Clone)]
-    enum TransactionRepeatType {
-        EveryDate(String),
-        EveryXDays(i32),
-    }
-
-    #[derive(Debug, Clone)]
-    struct Transaction {
-        value: f64,
-        target_account: i32,
-        source_account: Option<i32>,
-        date: NaiveDate,
-        repeat: Option<bool>,
-        repeat_mode: Option<TransactionRepeatType>,
-        auto_debt: Option<bool>
-    }
-
+fn main() {   
     #[derive(Debug, Clone)]
     struct FinanceManager {
-        pub persons: Vec<Person>,
-        pub accounts: Vec<Account>,
-        pub transactions: HashMap<Vec<Transaction>>,
-        pub next_id: i32
+        pub persons: HashMap<i32, Person>,
+        pub accounts: HashMap<i32, Account>,
+        pub transactions: HashMap<NaiveDate, Vec<Transaction>>,
+        id_generator: IdGenerator
     }
 
     impl FinanceManager {
         fn new() -> FinanceManager {
             let mut fin_manager = FinanceManager {
-                persons: Vec::new(),
-                accounts: Vec::new(),
-                transactions: Vec::new(),
-                next_id: 1
+                persons: HashMap::new(),
+                accounts: HashMap::new(),
+                transactions: HashMap::new(),
+                id_generator: IdGenerator::new()
             };
 
-            fin_manager.create_person(String::from("MainUser"));
+            fin_manager.create_person("MainUser");
+            fin_manager.create_account("wallet");
 
             fin_manager
         }
 
-        fn generate_id(&mut self) -> i32 {
-            let id = self.next_id;
-            self.next_id += 1;
-            id
-        }
-
-        fn create_person(&mut self, name: String) -> i32 {
+        fn create_person(&mut self, name: &str)  {
             let prs = Person {
-                id: self.generate_id(),
-                name: name,
+                id: self.id_generator.person(),
+                name: name.to_string(),
+                accounts: vec![],
             };
 
-            let prs_id = prs.id;
-
-            self.persons.push(prs);
-            
-            return prs_id;
+            self.persons.insert(prs.id.get_value(), prs);
 
         }
 
-        fn create_account(&mut self, title: String) -> i32 {
-            let buff = Account {
-                title: title,
-                id: self.generate_id(),
+        fn create_account(&mut self, title: &str) {
+            let acc = Account {
+                title: title.to_string(),
+                id: self.id_generator.account(),
+                owners: vec![1],
             };
 
-            let buff_id = buff.id;
+            for owner_id in acc.owners.iter() {
+                match self.persons.get_mut(owner_id) {
+                    Some(owner) => owner.accounts.push(acc.id.get_value()),
+                    None => println!("Person não encontrada"),
+                };
+            }
+            self.accounts.insert(acc.id.get_value(), acc);
 
-            self.accounts.push(buff);
-
-            return buff_id
         }
+
+        fn get_person_id_by_name(&self, name: &str) -> Option<i32> {
+            let mut id = None;
+            for person in self.persons.iter() {
+                if person.1.name == name {
+                    id = Some(*person.0);
+                    break;
+                }
+            }
+            return id;
+        }
+
+        fn get_person_by_id(&self, id: i32) -> Option<&Person> {
+            self.persons.get(&id)
+        }
+
+        fn list_accounts_from(&self, person_id: ID) -> Vec<ID> {
+            let mut res: Vec<ID> = vec![];
+            match self.persons.get(&person_id.get_value()) {
+                Some(person) => {
+                    person.accounts.iter().for_each(|x| res.push(ID::AccountID(*x)));
+                },
+                None => (),
+            };
+
+            return res;
+        }
+
+        fn create_deposit(&mut self, target_id: i32, amount: f64) {
+            let trs = Transaction {
+                value: amount,
+                id: self.id_generator.transaction(),
+                target_account: target_id,
+                source_account: None,
+                date: Utc::now(),
+            };
+
+            let date = trs.date.date_naive();
+
+            self.transactions.entry(date)
+                .or_insert_with(|| {vec![]})
+                .push(trs);
+        }
+
+        fn extract_date(&self, when: chrono::DateTime<Local>) -> NaiveDate {
+            let date = NaiveDate::from_ymd_opt(when.year(), when.month(), when.day()).expect("date");
+            return date
+        }
+
+        fn get_day_balance(&self, date: NaiveDate) -> HashMap<ID, Vec<ID>> {
+            let mut balance: HashMap<ID, Vec<ID>> = HashMap::new();
+            match self.transactions.get(&date) {
+                Some(list) => {
+
+                    for transaction in list.iter() {
+                        if let Some(account) = self.accounts.get(&transaction.target_account) {
+                            balance.entry(account.id)
+                                .or_insert_with(|| {vec![]})
+                                .push(transaction.id)
+                        }
+                    }
+                },
+                None => todo!(),
+            }
+
+            return balance;
+        }
+
+        fn get_transaction_by_id(&self, id: ID) -> Option<&Transaction> {
+            match id {
+                ID::TransactionID(_) => {
+                    for transactions in self.transactions.values() {
+                        if let Some(transaction) = transactions.iter().find(|t| t.id == id) {
+                            return Some(transaction)
+                        }
+                    }
+                    None
+                },
+                ID::PersonID(_) => panic!(),
+                ID::AccountID(_) => panic!(),
+            }
+        }
+
+        fn calc_transactions(&self, list: Vec<ID>) -> f64 {
+            let mut sum = 0.0;
+            for trs_id in list.iter() {
+                if let Some(trs) = self.get_transaction_by_id(*trs_id) {
+                    sum += trs.value;
+                }
+
+            }
+
+            return sum;
+        }
+
+        fn reduce_balance(&self, balance: HashMap<ID, Vec<ID>>) {
+            for (account, transactions) in balance {
+                println!("{} ${}", account.get_value(), self.calc_transactions(transactions))
+            }
+        }
+
     }
 
-    let core = FinanceManager::new();
-    println!("{:?}", core);
+    struct View;
+
+    impl View {
+        fn display_day_balance(fm: &FinanceManager) {
+            for (account_id, transactions_id) in fm.get_day_balance(Utc::now().date_naive()) {
+                println!("{} $ {}", fm.accounts.get(&account_id.get_value()).unwrap().title, fm.calc_transactions(transactions_id))
+            };
+        }
+    }
+    
+    let mut core = FinanceManager::new();
+
+    core.create_account("nubank");
+    core.create_deposit(1, 50.0);
+    core.create_account("bb");
+    core.create_deposit(3, -51.0);
+    core.create_deposit(1, -13.90);
+    core.create_deposit(2, 1723.5);
+    core.create_deposit(1, 50.0);
+    core.create_deposit(3, -51.0);
+
+    //println!("{:?}", core.persons.get(&1));
+    //println!("{:?}", core.list_accounts_from(ID::PersonID(1)));
+    //println!("{:?}", core.get_transaction_by_id(ID::TransactionID(1)));
+
+    //println!("{:?}", core.get_day_balance(Utc::now().date_naive()));
+    View::display_day_balance(&core);
 }
